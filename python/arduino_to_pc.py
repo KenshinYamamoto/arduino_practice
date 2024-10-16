2c1b22bc869aedfd2568a59decd3b47b0dc27b49
KenshinYamamoto <s2032149nb@s.chibakoudai.jp>
2024-10-16 11:49:14 +0900

    plot pressure sensor

--- python/arduino_to_pc.py
+++ python/arduino_to_pc.py
@@ -1,9 +1,30 @@
+import matplotlib.pyplot as plt
 import serial
+import time
+
 ser = serial.Serial('/dev/cu.usbmodem11301', 9600)
 not_used = ser.readline()
-while True:
+s_time = time.time()
+
+xs = []
+y = []
+counts = 0
+
+fig, ax = plt.subplots()
+ax.set_xlim(0, 300)
+ax.set_ylim(0, 1024)
+
+while time.time() - s_time < 10:
+    counts += 1
     val_arduino = ser.readline()
+    
+    xs.append(counts)
+    y.append(int(str(val_arduino).split("'")[1].split('\\')[0]))
+    
+    plt.pause(0.001)
     print(int(str(val_arduino).split("'")[1].split('\\')[0]))
     
 
+plt.plot(xs, y)
+plt.savefig('arduino_to_pc.png')
 ser.close()
