503b2b611f45fa5b4819dc11a4d2b5701fb58961
KenshinYamamoto <s2032149nb@s.chibakoudai.jp>
2024-10-16 11:40:52 +0900

    byte to int

--- python/arduino_to_pc.py
+++ python/arduino_to_pc.py
@@ -3,9 +3,7 @@ ser = serial.Serial('/dev/cu.usbmodem11301', 9600)
 not_used = ser.readline()
 while True:
     val_arduino = ser.readline()
-    # val_decoded = float(repr(val_arduino.decode())[1:-5])
-    val_decoded = val_arduino
-    print(val_decoded.decode())
+    print(int(str(val_arduino).split("'")[1].split('\\')[0]))
     
 
 ser.close()
